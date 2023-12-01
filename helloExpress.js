fae63c168e8cfcc3986eb629d9a6627ca08b9c41
choilhyun <cihg155@gmail.com>
2023-12-01 11:22:25 +0900

    exprsss 로 라우팅 처리1

--- helloExpress.js
+++ helloExpress.js
@@ -1,13 +1,27 @@
+const url = require("url");
 const express = require("express");
 const app = express();
 const port = 3000;
-
-app.get("/", (req, res) => {
-  res.set({ "Content-Type":"text/html; charset=utf-8"});
-  res.end("헬로 Express");
-});
 
 app.listen(port, ( ) =>{
   console.log(`Start server:use ${port}`);
 });
 
+// 라우팅 설정
+app.get("/", (_, res) => res.end("HOME"));
+app.get("/user", user);
+app.get("/feed", feed);
+
+function user(req, res){
+  const user = url.parse(req.url, true).query;
+  res.json(`[user] name : ${user.name}, age: ${user.age}`);
+}
+
+function feed(_, res){
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`<ul>
+        <li>picture1</li>
+        <li>picture2</li>
+        <li>picture3</li>
+  `);
+}
